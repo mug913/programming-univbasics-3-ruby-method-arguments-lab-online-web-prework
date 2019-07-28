72d138c575d54e650c90481890c3e3aa56eccb64
Brad Alexander <brad.alexander913@gmail.com>
2019-07-28 23:23:39 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,3 @@
-name = "Brad"
-language = "Ruby"
 
 def introduction (name)
   puts "Hi, my name is #{name}."
@@ -9,7 +7,7 @@ def introduction_with_language (name,language)
  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
-def introduction_with_language_optional (name, language)
+def introduction_with_language_optional (name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
