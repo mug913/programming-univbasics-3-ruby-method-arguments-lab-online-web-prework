62ee2dbf681504374d2d0c0b2dab75ecc2dbbf0b
Brad Alexander <brad.alexander913@gmail.com>
2019-07-28 23:21:22 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -10,6 +10,9 @@ def introduction_with_language (name,language)
 end
 
 def introduction_with_language_optional (name, language)
+  if language == NIL
+    language = "Ruby"
+  end
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
   
